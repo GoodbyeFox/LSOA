b2fa7541489a5d2721f2c5c4fd02cd2c1c34cf0e
Kanya6393 <kanya9901@gmail.com>
2024-01-27 11:43:52 +0700

    appmark

--- o2web/source/x_component_cms_FormDesigner/lp/la.js
+++ o2web/source/x_component_cms_FormDesigner/lp/la.js
@@ -2,7 +2,7 @@ MWF.xApplication.process = MWF.xApplication.process || {};
 MWF.xApplication.process.FormDesigner = MWF.xApplication.process.FormDesigner || {};
 MWF.xDesktop.requireApp("process.FormDesigner", "lp."+MWF.language, null, false);
 MWF.xApplication.cms.FormDesigner.LP = Object.merge( {}, MWF.xApplication.process.FormDesigner.LP, {
-    "selectApplication" : "选择应用",
+    "selectApplication" : "ເລືອກແອັບ",
     "formType": {
         "empty": "ຟອມວ່າງ",
         "publishEdit": "ແກ້ໄຂຟອມ",
