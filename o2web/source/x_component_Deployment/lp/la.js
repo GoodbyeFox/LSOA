bec77aec4a382caba1b64de0ef73d23d430c82f4
tedran <boomboomhan@gmail.com>
2024-02-29 17:57:10 +0700

    增加老挝语语言包

---
+++ o2web/source/x_component_Deployment/lp/la.js
@@ -0,0 +1,40 @@
+MWF.xApplication.Deployment.LP = {
+    "title": "ການປະຕິບັດ Application ",
+    "open": "ເປີດ",
+    "edit": "ຕັ້ງຄ່າ",
+    "remove": "ເອົາອອກ",
+    "add": "ປະຕິບັດ",
+
+    "name": "ຊື່",
+    "componentTitle": "ຫົວຂໍ້",
+    "path": "ເສັ້ນທາງ",
+    "isVisible": " ສາມາດເບິ່ງເຫັນ ໄດ້ ຫຼື ບໍ່",
+    "yes": "ແມ່ນ",
+    "no": "ບໍ່",
+    "widgetName": "widget ຊື່",
+    "widgetTitle": "widget ຫົວຂໍ້",
+    "widgetStart": "widget ເລີ່ມອັດຕະໂນມັດ",
+    "widgetVisible": "widget  ສາມາດເບິ່ງເຫັນ ໄດ້ ຫຼື ບໍ່ ",
+
+    "allowList": "ລາຍການທີ່ສາມາດເຂົ້າເຖິງໄດ້",
+    "denyList": "ປະຕິເສດການເຂົ້າເຖິງລາຍການ",
+    "controllerList": "ຜູ້ຈັດການ",
+    "selPerson": "ເລືອກພະນັກງານ",
+
+    "phone": "ໂທລະສັບ",
+    "mail": "mail",
+
+    "noInputInfor": "ກະລຸນາຕື່ມຂໍ້ມູນໃຫ້ຄົບຖ້ວນ。（ຕ້ອງຂຽນ：ຊື່, ຫົວຂໍ້, ເສັ້ນທງ）",
+    "deploySuccess": "ປະຕິບັດສຳເລັດ",
+    "modifySuccess": "ແກ້ໄຂ້ຂໍ້ມູນສຳເລັດ",
+
+    "removeComponentTitle": "ຢືນຢັນຍົກເລີກການຕິດຕັ້ງອົງປະກອບ",
+    "removeComponent": "ເຈົ້າແນ່ໃຈ ຫຼື ບໍ ວ່າຕ້ອງການຍົກເລີກການຕິດຕັ້ງອົງປະກອບ：{name} ？",
+    "removeComponentOk": "ຍົກເລີກການຕິດຕັ້ງສຳເລັດ",
+
+    "modify": "ແກ້ໄຂຂໍ້ມູນອົງປະກອບ",
+
+    "deployedComponent" : "ການປະຕິບັດອົງປະກອບສຳເລັດ",
+    "inputNameAndTitle" : "ກະລຸນາຢືນຢັນ ຊື່ application ແລະ ຫົວຂໍ້ application",
+    "uploadZipFile" : "ກະລຸນາອັບໂຫລດເອກະສານເປັນຟາຍ ZIP"
+};
